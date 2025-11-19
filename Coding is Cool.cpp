f5b9a7b4f08d280c80609557fcf08a757d27c79c
Project Specific Name <project-specific@example.com>
2025-11-19 23:50:45 +0000

    message

--- Coding is Cool.cpp
+++ Coding is Cool.cpp
@@ -1,6 +1,7 @@
 #include <iostream> 
 #include <string> 
 using namespace std;
+
 // Program to print a message indicating that coding is cool
 int main() { // Main function - execution starts here
     cout << "Coding is cool!" << endl; // Print a message indicating that coding is cool
